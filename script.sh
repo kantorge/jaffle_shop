e461c89688b7c8548fae8cd29ce1fe1309cae714
kantorge <kantorge@gmail.com>
2023-10-10 11:48:47 +0200

    experiment with cloud logger

--- script.sh
+++ script.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
-dbt debug --target cloudrun --profiles-dir .
-dbt run --target cloudrun --profiles-dir .
-dbt test --target cloudrun --profiles-dir .
+if dbt debug --target cloudrun --profiles-dir .; then
+  gcloud logging write dbt-jaffle-shop "dbt debug succeeded"
+else
+  gcloud logging write dbt-jaffle-shop "dbt debug failed"
+fi
+
+if dbt run --target cloudrun --profiles-dir .; then
+  gcloud logging write dbt-jaffle-shop "dbt run succeeded"
+else
+  gcloud logging write dbt-jaffle-shop "dbt run failed"
+fi
+
+if dbt test --target cloudrun --profiles-dir .; then
+  gcloud logging write dbt-jaffle-shop "dbt test succeeded"
+else
+  gcloud logging write dbt-jaffle-shop "dbt test failed"
+fi
