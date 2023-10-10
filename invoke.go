668d47bb422a0332aabb3eaacd544c1c2e8cafab
kantorge <kantorge@gmail.com>
2023-10-10 11:48:38 +0200

    GO file formatting

--- invoke.go
+++ invoke.go
@@ -6,23 +6,25 @@ import (
         "os"
         "os/exec"
 )
+
 func handler(w http.ResponseWriter, r *http.Request) {
-        log.Print("helloworld: received a request")
-cmd := exec.Command("/bin/sh", "script.sh")
+        log.Print("dbt-jaffle-shop: received a request")
+        cmd := exec.Command("/bin/sh", "script.sh")
         cmd.Stdout = os.Stdout
- cmd.Stderr = os.Stderr
- err := cmd.Run()
- if err != nil {
-  log.Fatalf("cmd.Run() failed with %s\n", err)
- }
+        cmd.Stderr = os.Stderr
+        err := cmd.Run()
+        if err != nil {
+                log.Fatalf("cmd.Run() failed with %s\n", err)
+        }
 }
+
 func main() {
-        log.Print("helloworld: starting server...")
-http.HandleFunc("/", handler)
-port := os.Getenv("PORT")
+        log.Print("dbt-jaffle-shop: starting server...")
+        http.HandleFunc("/", handler)
+        port := os.Getenv("PORT")
         if port == "" {
                 port = "8080"
         }
-log.Printf("helloworld: listening on %s", port)
+        log.Printf("dbt-jaffle-shop: listening on %s", port)
         log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
