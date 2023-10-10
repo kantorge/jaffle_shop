a9eaeb13e5945ad4ef250678c2a5097e27ae4e18
kantorge <kantorge@gmail.com>
2023-10-10 11:23:30 +0200

    adjust keyfile location for cloud run

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
-dbt debug --target dev --profiles-dir .
-dbt run --target dev --profiles-dir .
-dbt test --target dev --profiles-dir .
+dbt debug --target cloudrun --profiles-dir .
+dbt run --target cloudrun --profiles-dir .
+dbt test --target cloudrun --profiles-dir .
