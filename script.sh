4fe4de10e1a9c32544b99b285b319d5ad310efb9
kantorge <kantorge@gmail.com>
2023-10-10 11:14:35 +0200

    fix scripts

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
 dbt debug --target dev --profiles-dir .
 dbt run --target dev --profiles-dir .
-dbt test --data --target dev --profiles-dir .
+dbt test --target dev --profiles-dir .
